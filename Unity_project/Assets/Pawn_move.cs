7be002db8f3c386bb584009633eb3f13dbc14440
Qiwu Zou <zouqiwu@126.com>
2017-07-18 15:13:18 +0800

    Fix pawn

--- Unity_project/Assets/Pawn_move.cs
+++ Unity_project/Assets/Pawn_move.cs
@@ -6,25 +6,31 @@ public class Pawn_move : MonoBehaviour {
     private Rigidbody2D R2D;
     public float speed;
     public bool jumping = false;
+    public bool can_move = true;
     // Use this for initialization
     private void Awake()
     {
         R2D = GetComponent<Rigidbody2D>();
+        can_move = true;
+        tag = "Player";
     }
     void Start () {
-		
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        if (can_move)
+        {
+            R2D.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, R2D.velocity.y);
+            if (Input.GetAxis("Jump") == 1)
+                if (!jumping)
+                {
+                    R2D.AddForce(new Vector2(0, 50));
+                    jumping = true;
 
-        R2D.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, R2D.velocity.y);
-        if (Input.GetAxis("Jump") == 1)
-            if (!jumping) {
-                R2D.AddForce(new Vector2(0, 50));
-                jumping = true; 
-         
-            }
+                }
+        }
 	}
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -33,6 +39,8 @@ public class Pawn_move : MonoBehaviour {
 
     public void Death()
     {
+        tag = "Ghost";
+        can_move = false;
         Debug.Log("success");
     }
 }
